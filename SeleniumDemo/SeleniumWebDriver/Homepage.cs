99602f67d8364e193be8db8829b5d987052c9c84
Adam R. De King <adamdeking1@gmail.com>
2021-04-07 11:43:31 -0500

    Add Remove Element

--- SeleniumDemo/SeleniumWebDriver/Homepage.cs
+++ SeleniumDemo/SeleniumWebDriver/Homepage.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 
 namespace SeleniumWebDriver
@@ -85,5 +86,56 @@ namespace SeleniumWebDriver
             Assert.IsFalse(isImageLoaded, "Broken image shows.");
             Assert.IsTrue(isImageLoaded2, "Working image does not show");
         }
+
+        [Test]
+        public void AddRemoveElements()
+        {
+            driver.FindElement(addRemoveElement).Click();
+            Thread.Sleep(500);
+            // ensure 'Delete' button not present on page load.
+            try
+            {
+                bool deleteButton = driver.FindElement(By.ClassName("added-manually")).Displayed;
+                Assert.IsFalse(deleteButton, "Delete button present on page load.");
+            }
+            catch
+            {
+                Console.WriteLine("Delete button not present on page load, pass.");
+            }
+
+
+            // Click 'Add Element'
+            driver.FindElement(By.CssSelector("#content > div > button")).Click();
+            Thread.Sleep(500);
+
+            // ensure 'Delete' button now present.
+            try
+            {
+                bool deleteButton = driver.FindElement(By.ClassName("added-manually")).Displayed;
+                Console.WriteLine("Delete button present after clicking 'Add Element,' pass.");
+            }
+            catch
+            {
+                Assert.Fail("Delete Button not present");
+            }
+
+
+            // Click 'Delete'
+            driver.FindElement(By.ClassName("added-manually")).Click();
+            Thread.Sleep(500);
+
+            // Ensure 'Delete' button no longer displayed.
+            try
+            {
+                bool deleteButton = driver.FindElement(By.ClassName("added-manually")).Displayed;
+                Assert.IsFalse(deleteButton, "Delete button still present after 'delete' clicked.");
+            }
+            catch
+            {
+                Console.WriteLine("Delete button not present after 'delete' clicked, pass.");
+            }
+        }
+
+
     }
 }
