59cc7ddf3fdbb15b235470a2f1faa6e2438e1a0c
Adam R. De King <adamdeking1@gmail.com>
2021-04-12 13:11:17 -0500

    Added dbLogging framework

--- SeleniumDemo/SeleniumWebDriver/Pages/BrokenImagesPage.cs
+++ SeleniumDemo/SeleniumWebDriver/Pages/BrokenImagesPage.cs
@@ -10,29 +10,29 @@ namespace SeleniumWebDriver
     class BrokenImagesPage
     {
         public static void BrokenImagesTest(IWebDriver driver)
-        {
-            // Broken Image
-            var brokenImage = driver.FindElement(By.CssSelector("#content > div > img:nth-child(2)"));
+        {           
+                // Broken Image
+                var brokenImage = driver.FindElement(By.CssSelector("#content > div > img:nth-child(2)"));
 
-            Boolean isImageLoaded = (Boolean)((IJavaScriptExecutor)driver)
-                .ExecuteScript("return arguments[0].complete && typeof arguments[0]" +
-                ".naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", brokenImage);
-
-
-            Console.WriteLine("Broken Image should return false: " + isImageLoaded);
+                Boolean isImageLoaded = (Boolean)((IJavaScriptExecutor)driver)
+                    .ExecuteScript("return arguments[0].complete && typeof arguments[0]" +
+                    ".naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", brokenImage);
 
 
-            // Working image (banner "fork me on GitHub")
-            var workingImage = driver.FindElement(By.CssSelector("body > div:nth-child(2) > a > img"));
+                Console.WriteLine("Broken Image should return false: " + isImageLoaded);
 
-            Boolean isImageLoaded2 = (Boolean)((IJavaScriptExecutor)driver)
-                .ExecuteScript("return arguments[0].complete && typeof arguments[0]" +
-                ".naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", workingImage);
 
-            Console.WriteLine("Working Image should return true: " + isImageLoaded2);
+                // Working image (banner "fork me on GitHub")
+                var workingImage = driver.FindElement(By.CssSelector("body > div:nth-child(2) > a > img"));
 
-            Assert.IsFalse(isImageLoaded, "Broken image shows.");
-            Assert.IsTrue(isImageLoaded2, "Working image does not show");
+                Boolean isImageLoaded2 = (Boolean)((IJavaScriptExecutor)driver)
+                    .ExecuteScript("return arguments[0].complete && typeof arguments[0]" +
+                    ".naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", workingImage);
+
+                Console.WriteLine("Working Image should return true: " + isImageLoaded2);
+
+                Assert.IsFalse(isImageLoaded, "Broken image shows.");
+                Assert.IsTrue(isImageLoaded2, "Working image does not show");
         }
     }
 }
