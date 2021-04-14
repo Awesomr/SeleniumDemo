70423cf43c846e76fde5834703ad9629959b3b3c
Adam R. De King <adamdeking1@gmail.com>
2021-04-14 13:15:34 -0500

    Fixed branches

---
+++ SeleniumWebDriver/Homepage.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+using System.Configuration;
+
+
+namespace SeleniumWebDriver
+{
+    [TestFixture(Browser.Chrome)]
+    [TestFixture(Browser.Firefox)]
+    [TestFixture(Browser.IE)]
+    [Parallelizable]
+    public class Homepage : Setups
+    {
+        private Browser browser;
+        private string baseUrl = ConfigurationManager.AppSettings["baseUrl"];
+
+        private readonly By basicAuth = By.LinkText("Basic Auth");
+        private readonly By addRemoveElement = By.LinkText("Add/Remove Elements");
+        private readonly By brokenImages = By.LinkText("Broken Images");
+        private readonly By checkboxes = By.LinkText("Checkboxes");
+        private readonly By dropdown = By.LinkText("Dropdown");
+        private readonly By formAuthentication = By.LinkText("Form Authentication");
+
+        public Homepage(Browser browser)
+        {
+            
+        }
+
+        [Test]
+        public void BasicAuth()
+        {
+            DriverReset(browser);
+            driver.FindElement(basicAuth).Click();
+            BasicAuthPage.BasicAuth(driver);           
+        }
+
+        [Test]
+        public void BrokenImages()
+        {
+            DriverReset(browser);
+            driver.FindElement(brokenImages).Click();
+            BrokenImagesPage.BrokenImagesTest(driver);
+        }
+
+        [Test]
+        public void AddRemoveElements()
+        {
+            DriverReset(browser);
+            driver.FindElement(addRemoveElement).Click();
+            AddRemoveElementsPage.AddRemoveElementsTest(driver);
+        }
+
+        [Test]
+        public void Checkboxes()
+        {
+            DriverReset(browser);
+            driver.FindElement(checkboxes).Click();
+            CheckboxesPage.CheckboxesTest(driver);
+        }
+
+        [Test]
+        public void Dropdown()
+        {
+            DriverReset(browser);
+            driver.FindElement(dropdown).Click();
+            DropdownPage.DropdownTest(driver);
+        }
+
+        [Test]
+        public void FormAuthentication()
+        {
+            DriverReset(browser);
+            driver.FindElement(formAuthentication).Click();
+            FormAuthenticationPage.FormAuthenticationTest(driver);
+        }
+
+        public void DriverReset(Browser browser)
+        {
+            // Used for running multiple tests under same TestFixture
+            try
+            {
+                driver.Close();
+            }
+            catch
+            {
+                // do nothing
+            }
+            
+            driver = GetWebDriverForBrowser(browser);
+            driver.Manage().Window.Maximize();
+            driver.Url = baseUrl;
+        }
+    }
+}
---
+++ SeleniumWebDriver/Specflow/Steps/CraigslistDemoSteps.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SeleniumWebDriver.Specflow.Features
+{
+    [Binding]
+    public class CraigslistDemoSteps
+    {
+        CraigslistSearch craigslistSearch = null;
+
+        [Given(@"I launch the appliction")]
+        public void GivenILaunchTheAppliction()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = ConfigurationManager.AppSettings["craigslistUrl"];
+            craigslistSearch = new CraigslistSearch(driver);
+        }
+        
+        [Given(@"I enter '(.*)' into the searchbox")]
+        public void GivenIEnterIntoTheSearchbox(string p0)
+        {
+            craigslistSearch.EnterSearchTerm(p0);
+        }
+
+        [Given(@"I click 'has image'")]
+        public void GivenIClick()
+        {
+            craigslistSearch.ClickHasImage();
+        }
+        
+        [Given(@"I enter the price")]
+        public void GivenIEnterThePrice(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            craigslistSearch.EnterPriceRange((decimal)data.MinPrice, (decimal)data.MaxPrice);
+        }
+
+        [Given(@"I scroll down")]
+        public void GivenIScrollDown()
+        {
+            craigslistSearch.SearchFormPageDown();
+        }
+
+        [Given(@"I click the 'update search' button")]
+        public void GivenIClickTheButton()
+        {
+            craigslistSearch.ClickUpdateSearch();
+        }
+        
+        [Given(@"I click the top post")]
+        public void GivenIClickTheTopPost()
+        {
+            craigslistSearch.ClickTopPostOfUpdatedSearch();
+        }
+        
+        [Then(@"I should get the top price and ad information")]
+        public void ThenIShouldGetTheTopPriceAndAdInformation()
+        {
+            var price = craigslistSearch.GetListingPrice();
+            var ad = craigslistSearch.GetAdContent();
+
+            decimal decPrice = Convert.ToDecimal(price.Replace("$", ""));
+
+            Assert.IsTrue(decPrice < 501 && decPrice > 249);
+            Assert.IsTrue(ad != null);
+
+            
+        }
+        [Then(@"Close the browser")]
+        public void ThenCloseTheBrowser()
+        {
+            craigslistSearch.WebDriver.Dispose();            
+        }
+
+    }
+}
